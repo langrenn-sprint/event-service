17f829d21afa75e1264821b1acb3655d86eff774
Stig B. Dørmænen <stigbd@gmail.com>
2021-09-20 10:39:14 +0200

    fix: assign bibs according to raceclass order

--- tests/integration/test_assign_bibs_to_contestants.py
+++ tests/integration/test_assign_bibs_to_contestants.py
@@ -1,6 +1,7 @@
 """Integration test cases for the contestant route."""
 from datetime import date
 import os
+from typing import Any, List
 
 from aiohttp import hdrs
 from aiohttp.test_utils import TestClient as _TestClient
@@ -27,10 +28,48 @@ async def event() -> dict:
 
 
 @pytest.fixture
-async def contestant() -> dict:
-    """Create a mock contestant object."""
-    return {
-        "id": "290e70d5-0933-4af0-bb53-1d705ba7eb95",
+async def raceclasses() -> List[dict]:
+    """Create a mock raceclasses object."""
+    return [
+        {
+            "id": "11111111-11111-1111-1111-11111111111",
+            "name": "G12",
+            "ageclass_name": "G 12 år",
+            "event_id": "ref_to_event",
+            "order": 1,
+        },
+        {
+            "id": "99999999-99999-9999-9999-99999999999",
+            "name": "J12",
+            "ageclass_name": "J 12 år",
+            "event_id": "ref_to_event",
+            "order": 2,
+        },
+    ]
+
+
+@pytest.fixture
+async def raceclasses_without_order() -> List[dict]:
+    """Create a mock raceclasses object."""
+    return [
+        {
+            "id": "11111111-11111-1111-1111-11111111111",
+            "name": "G12",
+            "ageclass_name": "G 12 år",
+            "event_id": "ref_to_event",
+        },
+        {
+            "id": "99999999-99999-9999-9999-99999999999",
+            "name": "J12",
+            "ageclass_name": "J 12 år",
+            "event_id": "ref_to_event",
+        },
+    ]
+
+
+CONTESTANT_LIST = [
+    {
+        "id": "11111111-11111-1111-1111-11111111111",
         "first_name": "Cont E.",
         "last_name": "Stant",
         "birth_date": date(1970, 1, 1).isoformat(),
@@ -41,7 +80,39 @@ async def contestant() -> dict:
         "team": "Team Kollen",
         "email": "post@example.com",
         "event_id": "ref_to_event",
-    }
+    },
+    {
+        "id": "99999999-99999-9999-9999-99999999999",
+        "first_name": "Another Conte.",
+        "last_name": "Stant",
+        "birth_date": date(1980, 1, 1).isoformat(),
+        "gender": "F",
+        "ageclass": "J 12 år",
+        "region": "Oslo Skikrets",
+        "club": "Lyn Ski",
+        "team": "Team Kollen",
+        "email": "post@example.com",
+        "event_id": "ref_to_event",
+    },
+]
+
+
+@pytest.fixture
+async def contestants() -> List[dict]:
+    """Create a mock contestant object."""
+    return CONTESTANT_LIST
+
+
+def get_contestant_by_id(arg1: Any, event_id: str, contestant_id: str) -> dict:
+    """Look up correct contestant in list."""
+    return next(item for item in CONTESTANT_LIST if item["id"] == contestant_id)
+
+
+def update_contestant(
+    arg1: Any, event_id: str, contestant_id: str, contestant: dict
+) -> dict:
+    """Return the update contestant."""
+    return contestant
 
 
 @pytest.mark.integration
@@ -50,7 +121,8 @@ async def test_assign_bibs_to_contestants(
     mocker: MockFixture,
     token: MockFixture,
     event: dict,
-    contestant: dict,
+    raceclasses: List[dict],
+    contestants: List[dict],
 ) -> None:
     """Should return 201 Created, location header."""
     mocker.patch(
@@ -58,16 +130,20 @@ async def test_assign_bibs_to_contestants(
         return_value=event,
     )
     mocker.patch(
+        "event_service.adapters.raceclasses_adapter.RaceclassesAdapter.get_all_raceclasses",
+        return_value=raceclasses,
+    )
+    mocker.patch(
         "event_service.adapters.contestants_adapter.ContestantsAdapter.get_all_contestants",
-        return_value=[contestant],
+        return_value=contestants,
     )
     mocker.patch(
         "event_service.adapters.contestants_adapter.ContestantsAdapter.get_contestant_by_id",
-        return_value=contestant,
+        side_effect=get_contestant_by_id,
     )
     mocker.patch(
         "event_service.adapters.contestants_adapter.ContestantsAdapter.update_contestant",
-        return_value=contestant,
+        side_effect=update_contestant,
     )
 
     headers = MultiDict(
@@ -92,7 +168,8 @@ async def test_assign_bibs_to_contestants_event_not_found(
     client: _TestClient,
     mocker: MockFixture,
     token: MockFixture,
-    contestant: dict,
+    raceclasses: List[dict],
+    contestants: List[dict],
 ) -> None:
     """Should return 404 Not found."""
     mocker.patch(
@@ -100,8 +177,12 @@ async def test_assign_bibs_to_contestants_event_not_found(
         return_value=None,
     )
     mocker.patch(
+        "event_service.adapters.raceclasses_adapter.RaceclassesAdapter.get_all_raceclasses",
+        return_value=raceclasses,
+    )
+    mocker.patch(
         "event_service.adapters.contestants_adapter.ContestantsAdapter.get_all_contestants",
-        return_value=[contestant],
+        return_value=contestants,
     )
 
     headers = MultiDict(
@@ -118,12 +199,100 @@ async def test_assign_bibs_to_contestants_event_not_found(
         assert resp.status == 404
 
 
+@pytest.mark.integration
+async def test_assign_bibs_to_contestants_no_raceclasses(
+    client: _TestClient,
+    mocker: MockFixture,
+    token: MockFixture,
+    event: dict,
+    raceclasses: List[dict],
+    contestants: List[dict],
+) -> None:
+    """Should return 404 Not found."""
+    mocker.patch(
+        "event_service.adapters.events_adapter.EventsAdapter.get_event_by_id",
+        return_value=event,
+    )
+    mocker.patch(
+        "event_service.adapters.raceclasses_adapter.RaceclassesAdapter.get_all_raceclasses",
+        return_value=list(),
+    )
+    mocker.patch(
+        "event_service.adapters.contestants_adapter.ContestantsAdapter.get_all_contestants",
+        return_value=contestants,
+    )
+
+    headers = MultiDict(
+        {
+            hdrs.CONTENT_TYPE: "application/json",
+            hdrs.AUTHORIZATION: f"Bearer {token}",
+        },
+    )
+    event_id = event["id"]
+    with aioresponses(passthrough=["http://127.0.0.1"]) as m:
+        m.post("http://example.com:8081/authorize", status=204)
+        resp = await client.post(
+            f"/events/{event_id}/contestants/assign-bibs", headers=headers
+        )
+        assert resp.status == 404
+
+
+@pytest.mark.integration
+async def test_assign_bibs_to_contestants_raceclasses_without_order(
+    client: _TestClient,
+    mocker: MockFixture,
+    token: MockFixture,
+    event: dict,
+    raceclasses_without_order: List[dict],
+    contestants: List[dict],
+) -> None:
+    """Should return 400 Bad request."""
+    mocker.patch(
+        "event_service.adapters.events_adapter.EventsAdapter.get_event_by_id",
+        return_value=event,
+    )
+    mocker.patch(
+        "event_service.adapters.raceclasses_adapter.RaceclassesAdapter.get_all_raceclasses",
+        return_value=raceclasses_without_order,
+    )
+    mocker.patch(
+        "event_service.adapters.contestants_adapter.ContestantsAdapter.get_all_contestants",
+        return_value=contestants,
+    )
+    mocker.patch(
+        "event_service.adapters.contestants_adapter.ContestantsAdapter.get_contestant_by_id",
+        side_effect=get_contestant_by_id,
+    )
+    mocker.patch(
+        "event_service.adapters.contestants_adapter.ContestantsAdapter.update_contestant",
+        side_effect=update_contestant,
+    )
+
+    headers = MultiDict(
+        {
+            hdrs.CONTENT_TYPE: "application/json",
+            hdrs.AUTHORIZATION: f"Bearer {token}",
+        },
+    )
+    event_id = event["id"]
+    with aioresponses(passthrough=["http://127.0.0.1"]) as m:
+        m.post("http://example.com:8081/authorize", status=204)
+        resp = await client.post(
+            f"/events/{event_id}/contestants/assign-bibs", headers=headers
+        )
+        assert resp.status == 400
+
+
 # Unauthorized cases:
 
 
 @pytest.mark.integration
 async def test_assign_bibs_to_contestants_no_authorization(
-    client: _TestClient, mocker: MockFixture, event: dict
+    client: _TestClient,
+    mocker: MockFixture,
+    event: dict,
+    raceclasses: List[dict],
+    contestants: List[dict],
 ) -> None:
     """Should return 401 Unauthorized."""
     mocker.patch(
@@ -131,8 +300,12 @@ async def test_assign_bibs_to_contestants_no_authorization(
         return_value=event,
     )
     mocker.patch(
+        "event_service.adapters.raceclasses_adapter.RaceclassesAdapter.get_all_raceclasses",
+        return_value=raceclasses,
+    )
+    mocker.patch(
         "event_service.adapters.contestants_adapter.ContestantsAdapter.get_all_contestants",
-        return_value=[contestant],
+        return_value=contestants,
     )
 
     headers = MultiDict({hdrs.CONTENT_TYPE: "application/json"})
