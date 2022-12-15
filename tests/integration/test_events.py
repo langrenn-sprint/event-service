6158a5293e18b7de2603e2c90ad8dc3d35b37e96
Stig B. Dørmænen <stigbd@gmail.com>
2022-12-15 10:42:00 +0100

    Revert "Add support for using timezone from competition-format-service"
    This reverts commit da0047e7b4b9897cfae183d364acee496c84333e.

--- tests/integration/test_events.py
+++ tests/integration/test_events.py
@@ -56,7 +56,6 @@ async def competition_format() -> Dict[str, Union[int, str]]:
         "intervals": "00:00:30",
         "max_no_of_contestants_in_raceclass": 9999,
         "max_no_of_contestants_in_race": 9999,
-        "timezone": "Europe/Oslo",
         "datatype": "interval_start",
     }
 
